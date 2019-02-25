1eed5fd1eb8ab8d07c42ab62ee2f97d141f3b8a1
Alessandro Lascu <AlessandroL@icubed.it>
2019-02-25 16:48:39 +0100

    Added Back-End subnet

--- configuration.tf
+++ configuration.tf
@@ -21,5 +21,12 @@ resource "azurerm_subnet" "AZ-SUB-10-50-1-FRONT-END" {
   address_prefix       = "10.50.1.0/24"
   virtual_network_name = "AZ-VNET-10-50"
   resource_group_name  = "AZ-TF-RG"
-  #depends_on           = ["azurerm_virtual_network.vnet-10-50"]
+  depends_on           = ["azurerm_virtual_network.vnet-10-50"]
+}
+resource "azurerm_subnet" "AZ-SUB-10-50-2-BACK-END" {
+  name                 = "AZ-SUB-10-50-2-BACK-END"
+  address_prefix       = "10.50.2.0/24"
+  virtual_network_name = "AZ-VNET-10-50"
+  resource_group_name  = "AZ-TF-RG"
+  depends_on           = ["azurerm_virtual_network.vnet-10-50"]
 }
