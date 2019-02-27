b3e09c8619d2c88a1c830d364fcdaed4bdad2da4
Alessandro Lascu <AlessandroL@icubed.it>
2019-02-27 10:31:34 +0100

    Add custom DNS servers to VNet

--- vnet.tf
+++ vnet.tf
@@ -3,6 +3,7 @@ resource "azurerm_virtual_network" "vnet-10-50" {
   resource_group_name = "${azurerm_resource_group.resource_group_name_prod.name}"
   name                = "${var.vnet_info["name"]}"
   address_space       = ["${var.vnet_info["cidr"]}"]
+  dns_servers        = "${split(",", var.vnet_info["dns_servers"])}"
 
   tags {
     group       = "Terraform"
