b38bc296eeb1e0a9402c8fb0eacb52f9d00d5bca
Alessandro Lascu <AlessandroL@icubed.it>
2019-02-27 14:35:58 +0100

    Import route table resource

--- subnets.tf
+++ subnets.tf
@@ -3,6 +3,7 @@ resource "azurerm_subnet" "AZ-SUB-10-50-1-FRONT-END" {
   address_prefix       = "${lookup(var.subnet_info["front_end"],"cidr")}"
   virtual_network_name = "${azurerm_virtual_network.vnet-10-50.name}"
   resource_group_name  = "${azurerm_resource_group.resource_group_name_prod.name}"
+  route_table_id       = "${azurerm_route_table.imported_route_table.id}"
 }
 
 resource "azurerm_subnet" "AZ-SUB-10-50-2-BACK-END" {
