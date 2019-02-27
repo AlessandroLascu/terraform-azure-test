ed19ef09d8ecf3a698fb287b372d67817fc14790
Alessandro Lascu <AlessandroL@icubed.it>
2019-02-27 19:15:48 +0100

    Changed NSG rules

--- network_security_group.tf
+++ network_security_group.tf
@@ -10,7 +10,7 @@ resource "azurerm_network_security_group" "allow_RDP_nsg" {
     access                     = "Allow"
     protocol                   = "TCP"
     source_port_range          = "*"
-    destination_port_range     = "3389"
+    destination_port_range     = "3390"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
